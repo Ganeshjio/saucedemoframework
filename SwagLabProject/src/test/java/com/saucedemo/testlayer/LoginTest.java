5b1f04ffe09b565a4b0d02bcf6b12a34432e258e
purvapatil21 <purvap121@gmail.com>
2023-10-03 14:46:17 +0530

    Test case for login page

--- SwagLabProject/src/test/java/com/saucedemo/testlayer/LoginTest.java
+++ SwagLabProject/src/test/java/com/saucedemo/testlayer/LoginTest.java
@@ -1,5 +1,19 @@
 package com.saucedemo.testlayer;
 
-public class LoginTest {
+import org.testng.annotations.Test;
 
+import com.saucedemo.pagelayer.LoginPage;
+import com.saucedemo.testBase.TestBase;
+
+public class LoginTest extends TestBase {
+
+	@Test
+	
+	public void validateTheLoginPage()
+	{
+		LoginPage login_obj = new LoginPage(driver);
+		login_obj.enterUserName("standard_user");
+		login_obj.enterPassword("secret_sauce");
+		login_obj.clickOnLoginbtn();
+	}
 }
